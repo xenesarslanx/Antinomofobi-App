54e0c862da48b3012bd5e7b81f76546ac06735a7
xenesarslanx <enesarslan1664@gmail.com>
2021-07-27 17:36:52 +0300

    admob için ayar yapıldı

--- tasarim_calismasi-1/antinomofobi/lib/main.dart
+++ tasarim_calismasi-1/antinomofobi/lib/main.dart
@@ -1,26 +1,25 @@
 import 'package:antinomofobi/app_s%C3%BCreleri.dart';
 import 'package:antinomofobi/custom.dart';
 import 'package:antinomofobi/onboard.dart';
+import 'package:antinomofobi/services/advert-service.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 
-
 void main() {
-  runApp(ilk()
-  );
+  runApp(ilk());
 }
+
 class ilk extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:  AnimatedSplashScreen(
+      home: AnimatedSplashScreen(
         backgroundColor: Colors.black87,
         splash: Image.asset('lib/assets/logoEApp2.png'),
-        nextScreen:MyApp(),
-         splashIconSize: 400,
+        nextScreen: MyApp(),
+        splashIconSize: 400,
         splashTransition: SplashTransition.scaleTransition,
       ),
-  
     );
   }
 }
@@ -31,79 +30,86 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final AdvertService _advertService = AdvertService(); //admob
+  @override
+  void initState() {
+    _advertService.showIntersitial();
+    _advertService.showBanner();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-  return MaterialApp(
-    
-    theme: ThemeData(
-      textTheme: TextTheme(
-        headline1: TextStyle(fontSize:40, fontWeight: FontWeight.bold, color: Colors.white),
-        headline5: TextStyle(fontSize:14, fontWeight: FontWeight.bold, color: Colors.white60),
-        bodyText1: TextStyle(fontSize:14, color: Colors.white),
-        bodyText2: TextStyle(fontSize:20, color: Colors.white),
-
-        
-      )
-    ),
+    return MaterialApp(
+      theme: ThemeData(
+          textTheme: TextTheme(
+        headline1: TextStyle(
+            fontSize: 40, fontWeight: FontWeight.bold, color: Colors.white),
+        headline5: TextStyle(
+            fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white60),
+        bodyText1: TextStyle(fontSize: 14, color: Colors.white),
+        bodyText2: TextStyle(fontSize: 20, color: Colors.white),
+      )),
       home: Scaffold(
-    
- body:SafeArea(
-    child: PageView.builder(
-       itemCount: 2,
-       itemBuilder: (BuildContext context, int index){
-         OnboardingItem oi = OnboardingItems.loadOnboardingItem()[index];
-         return Container(
-           height: MediaQuery.of(context).size.height,
-           width: MediaQuery.of(context).size.width,
-           color:oi.color,
-           padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-
-        child:Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          
-          children: <Widget>[
-            Column(
-             
-              children: [
-                ElevatedButton(
-                     child:Text('GEÇ', style: Theme.of(context).textTheme.headline5),
-                  onPressed:() => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) =>ilkSayfa(),
-                    
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-                Container(child: Text('HOŞGELDİNİZ'),)
-                ],
-            ),
-            HeroImage(
-              img: oi.image, 
-              imgHeight:MediaQuery.of(context).size.height*0.4,),
-        
-        Column(
-          children: [
-            Text(oi.title, style: Theme.of(context).textTheme.headline1,),
-            SizedBox(height: 10,),
-            Text(oi.subtitle, style: Theme.of(context).textTheme.bodyText1),
-            SizedBox(height: 40),
-   
-          index == (2-1)?//2 sayfa sayısıdır
-            CustomButton(btnText: 'BAŞLA-->', ) : Container()
-          ],
-        ),
-        ],
-        )
-         );
-       }
-       
-       ),
- )   
-         
-     ),
-  );
+          body: SafeArea(
+        child: PageView.builder(
+            itemCount: 2,
+            itemBuilder: (BuildContext context, int index) {
+              OnboardingItem oi = OnboardingItems.loadOnboardingItem()[index];
+              return Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  color: oi.color,
+                  padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Column(
+                        children: [
+                          ElevatedButton(
+                            child: Text('GEÇ',
+                                style: Theme.of(context).textTheme.headline5),
+                            onPressed: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ilkSayfa(),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Container(
+                            child: Text('HOŞGELDİNİZ'),
+                          )
+                        ],
+                      ),
+                      HeroImage(
+                        img: oi.image,
+                        imgHeight: MediaQuery.of(context).size.height * 0.4,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            oi.title,
+                            style: Theme.of(context).textTheme.headline1,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(oi.subtitle,
+                              style: Theme.of(context).textTheme.bodyText1),
+                          SizedBox(height: 40),
+                          index == (2 - 1)
+                              ? //2 sayfa sayısıdır
+                              CustomButton(
+                                  btnText: 'BAŞLA-->',
+                                )
+                              : Container()
+                        ],
+                      ),
+                    ],
+                  ));
+            }),
+      )),
+    );
   }
-  }
- 
+}
---
+++ tasarim_calismasi-1/antinomofobi/lib/services/advert-service.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_admob/firebase_admob.dart';
+
+class AdvertService {
+  static final AdvertService _instance = AdvertService._internal();
+  factory AdvertService() => _instance;
+  MobileAdTargetingInfo _targetingInfo;
+  final String _gecis = "ca-app-pub-5417429060364094/5128942409";
+  final String _banner = "ca-app-pub-5417429060364094/1215911260";
+  AdvertService._internal() {
+    _targetingInfo = MobileAdTargetingInfo();
+  }
+  showBanner() {
+    BannerAd banner = BannerAd(
+        adUnitId: BannerAd.testAdUnitId,
+        size: AdSize.smartBanner,
+        targetingInfo: _targetingInfo);
+
+    banner
+      ..load()
+      ..show();
+
+    banner.dispose();
+  }
+
+  showIntersitial() {
+    InterstitialAd interstitialAd =
+        InterstitialAd(adUnitId: _gecis, targetingInfo: _targetingInfo);
+
+    interstitialAd
+      ..load()
+      ..show(anchorType: AnchorType.top, anchorOffset: 30);
+
+    interstitialAd.dispose();
+  }
+}
